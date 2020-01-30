29cf57f7c51844eaf47d50e2ee6c3a98e63c7087
Srijith <srijiths1998@gmail.com>
2020-01-30 17:57:37 +0530

    Added loading gif and reset button

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 document.getElementById('convert').addEventListener('click', function(e) {
     // Get temperature value from input field
-    const tempField = document.getElementById('temp-value')
-    const tempInCelsius = parseFloat(tempField.value);
-    console.log(tempInCelsius);
+    let tempInCelsius = document.getElementById('temp-value').value;
     
+    // Validate input
+    if(tempInCelsius === '') {
+        alert("Please enter a value..");
+        window.location.reload();
+    }
+    else {
+    //Display loading symbol
+    document.getElementById('loading').style.display = 'block';
+    e.target.textContent = 'Converting..';
+
+    //call converTemperature after 2s
+    setTimeout(convertTemperature, 1500);
+
+    }
+    e.preventDefault();
+})
+
+
+function convertTemperature() {
+    //Hide loading symbol
+    document.getElementById('loading').style.display = 'none';
+
+    //Get input value
+    const tempInCelsius = parseFloat(document.getElementById('temp-value').value);
     //Calculate F and K
     const farenheit = (tempInCelsius * 9/5) + 32;
     const kelvin = tempInCelsius + 273.15;
@@ -11,12 +33,17 @@ document.getElementById('convert').addEventListener('click', function(e) {
     const fOutput = document.getElementById('temp-farenheit');
     const kOutput = document.getElementById('temp-kelvin');
 
-    fOutput.value = farenheit;
-    kOutput.value = kelvin;
+    fOutput.value = `${farenheit} °F`;
+    kOutput.value = `${kelvin} K`;
 
-    const output = document.querySelector('.results');
-    output.style.display = 'block';
+    //Display output
+    document.querySelector('.results').style.display = 'block';
+    
+    //Display reset button
+    button = document.getElementById('convert');
+    button.textContent = 'Reset';
+    button.addEventListener('mousedown', function(e) {
+        window.location.reload();
+    })
 
-    tempField.value = '';
-    e.preventDefault();
-})
+}
